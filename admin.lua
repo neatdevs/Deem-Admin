84ce3bb726ab27545ca8a7235a3eb1c80a8bbf55
Teemsploit <53855235+Teemsploit@users.noreply.github.com>
2024-12-18 01:44:14 +0000

    Update admin.lua

--- admin.lua
+++ admin.lua
@@ -29,7 +29,6 @@ function readfileExploit()
 end
 
 -- Locals
-
 local cloneref = cloneref or function(o) return o end
 COREGUI = cloneref(game:GetService("CoreGui"))
 Players = cloneref(game:GetService("Players"))
